9f5ad5278d9685901ff720303e325eba15b33a52
Dandan <ddliu@thoughtworks.com>
2020-07-03 22:29:47 +0800

    complete practice

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,6 @@
-export const getPoetry
+export const getPoetry = async () => {
+  const dataJson = await fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+  return [dataJson.origin, dataJson.author, dataJson.content];
+};
